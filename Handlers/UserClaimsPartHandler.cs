d1dab5c5d25c492bc5fe0f4324ac223455a683ca
Alex Petryakov <alexpetryakov@gmail.com>
2016-05-31 16:10:01 +0300

    add claims part for openIdConnect (IdentityServer3)

---
+++ Handlers/UserClaimsPartHandler.cs
@@ -0,0 +1,13 @@
+﻿using Orchard.ContentManagement;
+using Orchard.Data;
+using Orchard.ContentManagement.Handlers;
+using MainBit.Users.Models;
+
+namespace MainBit.Users.Handlers {
+    public class UserClaimsPartHandler : ContentHandler {
+        public UserClaimsPartHandler(IRepository<UserPersonalDataPartRecord> repository)
+        {
+            Filters.Add(new ActivatingFilter<UserClaimsPart>("User"));
+        }
+    }
+}
---
+++ Models/UserClaimsPart.cs
@@ -0,0 +1,48 @@
+﻿using Orchard.ContentManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MainBit.Users.Models
+{
+    public class UserClaimsPart : ContentPart
+    {
+        public string AccessToken
+        {
+            get { return this.Retrieve(x => x.AccessToken); }
+            set { this.Store(x => x.AccessToken, value); }
+        }
+
+        public string ExpiresAt
+        {
+            get { return this.Retrieve(x => x.ExpiresAt); }
+            set { this.Store(x => x.ExpiresAt, value); }
+        }
+
+        public string RefreshToken
+        {
+            get { return this.Retrieve(x => x.RefreshToken); }
+            set { this.Store(x => x.RefreshToken, value); }
+        }
+
+        public string IdentityToken
+        {
+            get { return this.Retrieve(x => x.IdentityToken); }
+            set { this.Store(x => x.IdentityToken, value); }
+        }
+
+        public string Subject
+        {
+            get { return this.Retrieve(x => x.Subject); }
+            set { this.Store(x => x.Subject, value); }
+        }
+
+        public string SessionId
+        {
+            get { return this.Retrieve(x => x.SessionId); }
+            set { this.Store(x => x.SessionId, value); }
+        }
+
+    }
+}
